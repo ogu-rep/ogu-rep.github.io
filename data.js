f2013caacd5cc2395f1b0f48819ef79ed5ce2439
sogu <66258304+ogu-rep@users.noreply.github.com>
2021-01-12 16:04:26 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,85 @@
+'use strict';
+
+// マスタ
+const data = [
+  {
+    url: "https://ogu-rep.github.io/css-popup/",
+    name: "Css Popup",
+    useLang: ["html", "CSS"],
+  },
+  {
+    url: "https://ogu-rep.github.io/awesome-app/",
+    name: "Awesome App",
+    useLang: ["html", "CSS"],
+  },
+  {
+    url: "https://ogu-rep.github.io/modal/",
+    name: "Modal",
+    useLang: ["html", "CSS", "Javascript"],
+  },
+  {
+    url: "https://ogu-rep.github.io/tab-menu/",
+    name: "Tab Menu",
+    useLang: ["html", "CSS", "Javascript"],
+  },
+  {
+    url: "https://ogu-rep.github.io/hamburger-menu/",
+    name: "Hamburger Menu",
+    useLang: ["html", "CSS", "Javascript"],
+  },
+  {
+    url: "https://ogu-rep.github.io/stop-watch/",
+    name: "Stop Watch",
+    useLang: ["html", "CSS", "Javascript"],
+  },
+  {
+    url: "https://ogu-rep.github.io/slideshow/",
+    name: "Slide Show",
+    useLang: ["html", "CSS", "Javascript"],
+  },
+  {
+    url: "https://ogu-rep.github.io/calendar/" ,
+    name: "Calendar",
+    useLang: ["html", "CSS", "Javascript"],
+  },
+  {
+    url: "https://ogu-rep.github.io/win-and-lose/",
+    name: "Win and Lose",
+    useLang: ["html", "CSS", "Javascript"],
+  },
+  {
+    url: "https://ogu-rep.github.io/omikuzi/",
+    name: "Omikuzi",
+    useLang: ["html", "CSS", "Javascript"],
+  },
+  {
+    url: "https://ogu-rep.github.io/bingo/" ,
+    name: "Bingo",
+    useLang: ["html", "CSS", "Javascript"],
+  },
+  {
+    url: "https://ogu-rep.github.io/typing/" ,
+    name: "Typing",
+    useLang: ["html", "CSS", "Javascript"],
+  },
+  {
+    url: "https://ogu-rep.github.io/color-touch/" ,
+    name: "Color Touch Counter",
+    useLang: ["html", "CSS", "Javascript", "React"],
+  },
+  {
+    url: "https://ogu-rep.github.io/todo/" ,
+    name: "Todo List",
+    useLang: ["html", "CSS", "Javascript", "React"],
+  },
+  {
+    url: "https://github.com/ogu-rep/my-sql",
+    name: "stored procedure",
+    useLang: ["MySQL"],
+  },
+  {
+    url: "https://ogu-rep.github.io/ajax-example/",
+    name: "ajax example",
+    useLang: ["html", "CSS", "Javascript"],
+  },
+];
